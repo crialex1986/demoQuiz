2b690f0c051133fe6d69f1c1fe2e8680e28d12c3
Cristian Salazar <estuam@UAM.LOCAL>
2019-04-06 11:02:20 -0500

    Se agregó comando fallback

--- routes/botman.php
+++ routes/botman.php
@@ -8,13 +8,26 @@ $botman->hears('/start', function ($bot) {
 	$bot->reply('Hola ' . $nombres . ', bienvenido al bot SimpleQuizzes!');
 });
 
+$botman->hears('/ayuda', function ($bot) {
+	$ayuda = ['/ayuda' => 'Mostrar este mensaje de ayuda',
+          	'acerca de|acerca' => 'Ver la información quien desarrollo este lindo bot',
+          	'listar quizzes|listar' => 'Listar los cuestionarios disponibles',
+          	'iniciar quiz <id>' => 'Iniciar la solución de un cuestionario',
+          	'ver puntajes|puntajes' => 'Ver los últimos puntajes',
+          	'borrar mis datos|borrar' => 'Borrar mis datos del sistema'];
+    
+	$bot->reply("Los comandos disponibles son:");
+
+	foreach($ayuda as $key=>$value)
+	{
+    		$bot->reply($key . ": " . $value);
+	}
+});
+
 //Comportamiento por defecto
 $botman->fallback(function ($bot) {
 	$bot->reply("No entiendo que quieres decir, vuelve a intentarlo.");
 });
-
-
-
 /*
 $botman->hears('Hi', function ($bot) {
     $bot->reply('Hello!');
