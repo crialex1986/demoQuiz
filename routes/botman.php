2b69ebabdb646d0be2745b55f3993b1e5346f703
Cristian Salazar <estuam@UAM.LOCAL>
2019-04-06 10:59:17 -0500

    Modificación de escenarios básicos

--- routes/botman.php
+++ routes/botman.php
@@ -8,17 +8,16 @@ $botman->hears('/start', function ($bot) {
 	$bot->reply('Hola ' . $nombres . ', bienvenido al bot SimpleQuizzes!');
 });
 
-
-
-
-
-
-$botman->hears('Hi', function ($bot) {
-    $bot->reply('Hello!');
-});
-$botman->hears('Start conversation', BotManController::class.'@startConversation');
-
 //Comportamiento por defecto
 $botman->fallback(function ($bot) {
 	$bot->reply("No entiendo que quieres decir, vuelve a intentarlo.");
 });
+
+
+
+/*
+$botman->hears('Hi', function ($bot) {
+    $bot->reply('Hello!');
+});
+$botman->hears('Start conversation', BotManController::class.'@startConversation');
+*/
